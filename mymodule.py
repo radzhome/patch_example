a0100ce20f2c92f2e1cd909c4dd481dca2c37494
rad <radzhome@gmail.com>
2017-01-10 16:44:06 -0500

    add removal, upload as a service

--- mymodule.py
+++ mymodule.py
@@ -2,7 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import os
+import os.path
 
 
-def rm(filename):
-    os.remove(filename)
+class RemovalService(object):
+    """A service for removing objects from the filesystem."""
+
+    @staticmethod
+    def rm(filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+
+class UploadService(object):
+    def __init__(self, removal_service):
+        self.removal_service = removal_service
+
+    def upload_complete(self, filename):
+        self.removal_service.rm(filename)
--- test_rm_w_mock.py
+++ test_rm_w_mock.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from mymodule import rm
+from mymodule import RemovalService
 
 import mock
 import unittest
-# See https://www.toptal.com/python/an-introduction-to-mocking-in-python
-# sudo pip install mock  (for py 2.7)
 
 
-# Mock an item where it is used, not where it came from.
-class RmTestCase(unittest.TestCase):
+class RemovalServiceTestCase(unittest.TestCase):
+    @mock.patch('mymodule.os.path')
     @mock.patch('mymodule.os')
-    def test_rm(self, mock_os):
-        rm("any path")
-        # test that rm called os.remove with the right parameters
+    def test_rm(self, mock_os, mock_path):
+        # instantiate our service
+        reference = RemovalService()
+
+        # set up the mock
+        mock_path.isfile.return_value = False
+
+        reference.rm("any path")
+
+        # test that the remove call was NOT called.
+        self.assertFalse(mock_os.remove.called, "Failed to not remove the file if not present.")
+
+        # make the file 'exist'
+        mock_path.isfile.return_value = True
+
+        reference.rm("any path")
+
         mock_os.remove.assert_called_with("any path")
